98a0b48bd37a67b3257564f0559253be3cce19c1
amin farahani <aminfarahanim@gmail.com>
2025-05-18 08:40:37 +0330

    Add project files.

---
+++ NumberplateRecognition/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading;
+using System.Threading.Channels;
+using NumberplateRecognition;
+using OpenCvSharp;
+
+public class Program
+{
+    public static void main()
+    {
+        string[] inputCamURLs = { };
+        string[] outputCamURLs = { };
+
+        for (int x = 1; x < inputCamURLs.Length; x++)
+        {
+            var channel = Channel.CreateUnbounded<Record>();
+
+            Task.Run(async () =>
+            {
+                var capture = new VideoCapture(inputCamURLs[x]);
+                if (!capture.IsOpened())
+                {
+                    Console.WriteLine("Connection failed!");
+                }
+
+                var frame = new Mat();
+
+                while (true)
+                {
+                    bool success = capture.Read(frame);
+                    if (success && !frame.Empty())
+                    {
+                        var record = new Record(frame.Clone(), x);
+                        await Task.Delay(450);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Frame is empty!");
+                    }
+                }
+            });
+
+            Task.Run(() =>
+            {
+
+            });  
+        }
+        
+
+    }
+}
---
+++ NumberplateRecognition/TempLib.cs
@@ -0,0 +1,22 @@
+﻿using OpenCvSharp;
+
+namespace NumberplateRecognition
+{
+    public class Record
+    {
+        public Mat Frame { get; set; }
+
+        public int CameraID { get; set; }
+
+        public Record(Mat frame, int id)
+        {
+            Frame = frame;
+            CameraID = id;
+        }
+
+        public void Dispose()
+        {
+            Frame?.Dispose();
+        }
+    }
+}
