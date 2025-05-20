44e38992d1a02e2b40efbb290636869788fa3193
amin farahani <aminfarahanim@gmail.com>
2025-05-20 07:49:25 +0330

    Model is stable; but Cv2 can't read images. Structure with threads is OK, tasks need debugging and review.

--- NumberplateRecognition/Program.cs
+++ NumberplateRecognition/Program.cs
@@ -1,51 +1,72 @@
-﻿using System.Threading.Channels;
+﻿using NumberplateRecognition;
 using OpenCvSharp;
+using System.Threading.Channels;
 
-namespace NumberplateRecognition
-{
-    public class Program
+
+List<string> inputCamURLs = [ "rtsp://:8554/test" ];
+List<string> outputCamURLs = [];
+List<Task> tasks = [];
+
+var model = new Model("E:\\yolo11n.onnx");
+Thread.Sleep(5000);
+List<string> pathes = new List<string>() { "E:\\a.jpg", "E:\\b.jpg", "E:\\c.jpg", "E:\\d.jpg", "E:\\e.jpg" };
+
+
+//for (int x = 0; x < inputCamURLs.Length; x++)
+//{
+    var channel = Channel.CreateUnbounded<Record>();
+
+    // var t1 = Task.Run(async () =>
+    Thread t1 = new Thread(async () =>
     {
-        public static void Main()
+        foreach(var path in pathes)
         {
-            string[] inputCamURLs = { };
-            string[] outputCamURLs = { };
-
-            for (int x = 1; x < inputCamURLs.Length; x++)
-            {
-                var channel = Channel.CreateUnbounded<Record>();
-
-                Task.Run(async () =>
-                {
-                    var capture = new VideoCapture(inputCamURLs[x]);
-                    if (!capture.IsOpened())
-                    {
-                        Console.WriteLine("Connection failed!");
-                    }
-
-                    var frame = new Mat();
-
-                    while (true)
-                    {
-                        bool success = capture.Read(frame);
-                        if (success && !frame.Empty())
-                        {
-                            var record = new Record(frame.Clone(), x);
-                            await Task.Delay(450);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Frame is empty!");
-                        }
-                    }
-                });
-
-                Task.Run(() =>
-                {
-
-                });
-            }
-
-
+            var frame = Cv2.ImRead(path);
+            var record = new Record(frame.Clone(), 1);
         }
-    }
-}
+
+        //var capture = new VideoCapture(inputCamURLs[x]);
+        //if (!capture.IsOpened())
+        //{
+        //    Console.WriteLine("Connection failed!");
+        //}
+
+        //while (true)
+        //{
+            //bool success = capture.Read(frame);
+            //if (success && !frame.Empty())
+            //{
+            //    var record = new Record(frame.Clone(), x);
+            //    await channel.Writer.WriteAsync(record);
+            //    await Task.Delay(450);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Reading stream failed or frame is empty!");
+            //}
+        //}
+    });
+    //tasks.Add(t1);
+
+    //var t2 = Task.Run(async () =>
+    Thread t2 = new Thread(async () =>
+    {
+        var model = new Model("E:\\yolo11n.onnx");
+
+        while (true)
+        {
+            var record = await channel.Reader.ReadAsync();
+            Cv2.CvtColor(record.Frame, record.Frame, ColorConversionCodes.BGR2RGB);
+            model.DetectTruck(record.Frame);
+        }
+    });
+    //tasks.add(t2);
+//}
+
+// Task.WhenAll(tasks).GetAwaiter().GetResult();
+
+t1.Start();
+t2.Start();
+
+t1.Join();
+t2.Join();
--- NumberplateRecognition/TempLib.cs
+++ NumberplateRecognition/TempLib.cs
@@ -1,4 +1,7 @@
-﻿using OpenCvSharp;
+﻿using System.Diagnostics;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using OpenCvSharp;
 
 namespace NumberplateRecognition
 {
@@ -20,5 +23,55 @@ namespace NumberplateRecognition
         }
     }
 
+    public class Model
+    {
+        public InferenceSession Session { get; set; }
 
+        public Model(string modelPath)
+        {
+            Session = new InferenceSession(modelPath);
+            Console.WriteLine("Model loaded correctly.");
+        }
+
+        public bool DetectTruck(Mat frame)
+        {
+            var image = new Mat();
+            Cv2.Resize(frame, image, new Size(960, 640));
+
+            var tensor = new DenseTensor<float>([1, 3, 640, 960]);
+
+            for (int h = 0; h < 640; h++)
+            {
+                for (int w = 0; w < 960; w++)
+                {
+                    var pixel = image.At<Vec3b>(h, w);
+                    tensor[0, 0, h, w] = pixel.Item0 / 255.0f;
+                    tensor[0, 1, h, w] = pixel.Item1 / 255.0f;
+                    tensor[0, 2, h, w] = pixel.Item2 / 255.0f;
+                }
+            }
+
+            image.Dispose();
+            var input = new List<NamedOnnxValue> { NamedOnnxValue.CreateFromTensor("images", tensor) };
+
+            var timer = Stopwatch.StartNew();
+            using (var output = Session.Run(input))
+            {
+                timer.Stop();
+                Console.WriteLine(timer.ElapsedMilliseconds);
+                var result = output.First().AsTensor<float>();
+                for (int b = 0; b < result.Dimensions[1]; b++)
+                {
+                    if (result[0, b, 4] > 0.5 && result[0, b, 12] > 0.55)
+                    {
+                        Console.WriteLine("truck found");
+                        return true;
+                    }
+                }
+            }
+
+            Console.WriteLine("truck not found");
+            return false;
+        }
+    }
 }
